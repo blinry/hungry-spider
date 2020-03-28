648bf4afa4b52ae5c2ae267e8e5e88a3e61e6b26
Sebastian Morr <sebastian@morr.cc>
2020-03-28 17:13:52 +0100

    Walking around

--- leg.gd
+++ leg.gd
@@ -3,15 +3,26 @@ extends Spatial
 var l = 1 # limb length
 
 export var target = Vector3(0, 0, 0)
+var internal_target = Vector3(0, 0, 0)
 
 func _ready():
     pass
 
 func _process(delta):
-    $Pivot.look_at(target, Vector3(0, 1, 0))
-    var d = (target - get_global_transform().origin).length()
+    internal_target = lerp(internal_target, target, 0.4)
+    $Pivot.look_at(internal_target, Vector3(0, 1, 0))
+    var d = internal_distance()
     var alpha = 0
     if d <= 2*l:
         alpha = acos(d/(2*l))
     $Pivot/UpperLeg.rotation = Vector3(-alpha, PI, 0)
     $Pivot/UpperLeg/LowerLeg.rotation = Vector3(2*alpha, 0, 0)
+
+func distance():
+    return (target - get_global_transform().origin).length()
+
+func internal_distance():
+    return (internal_target - get_global_transform().origin).length()
+
+func rot():
+    return $Pivot.rotation.y
--- main.gd
+++ main.gd
@@ -2,15 +2,33 @@ extends Spatial
 
 onready var camera = $Camera
 
+var legs = []
+
+var maxd = 2
+var mind = 0.5
+var maxrot = PI/4
+var radius = 1.3
+
 func _ready():
     var leg = preload("res://leg.tscn")
-    for i in range(1,8):
+    for i in range(0,8):
         var l = leg.instance()
-        l.target = Vector3(sin(i/8.0*2*PI), 0, cos(i/8.0*2*PI))
+        l.target = Vector3(sin(i/8.0*2*PI)*radius, 0, cos(i/8.0*2*PI)*radius)
+        l.rotation.y = PI+atan2(l.target.x, l.target.z)
         $Spider.add_child(l)
+        legs.push_back(l)
     
 func _process(delta):
-    var plane = Plane(Vector3(0, 0, 1), 0)
+    var plane = Plane(Vector3(0, 1, 0), 0.4)
     var mouse = get_viewport().get_mouse_position()
     var pos = plane.intersects_ray(camera.project_ray_origin(mouse), camera.project_ray_normal(mouse))
-    $Spider.translation = pos
+    
+    if pos:
+        $Spider.translation = lerp($Spider.translation, pos, 0.3)
+    
+    for i in range(legs.size()):
+        var l = legs[i]
+        if l.distance() > maxd or l.distance() < mind or abs(l.rot()) > maxrot:
+            var new_target = Vector3($Spider.translation.x+sin(i/8.0*2*PI)*radius, 0, $Spider.translation.z+cos(i/8.0*2*PI)*radius)
+            #$Tween.interpolate_property(l, "target", l.target, , 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+            l.target = new_target
